d6be9f507411d7afa35e7454c84e5ed7a5833428
aleksandershibecky <ejykvip@gmail.com>
2022-03-28 14:29:59 +0300

    home work2.3

--- traning1.py
+++ traning1.py
@@ -1,2 +1,3 @@
 print (8 / 2 + 5 - -3 / 2)
 print ("\"Khal Drogo's favorite word is \"athjahakar\"\"")
+print ("Winter" + " " + "came" + " " + "for" + " " + "the" + " " + "House" + " " + "of" + " " + "Frey"".")
