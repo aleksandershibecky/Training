31ff8548db80d0233ae041aef422069ec9e92050
aleksandershibecky <ejykvip@gmail.com>
2022-03-28 15:00:11 +0300

    home work2.5

--- traning1.py
+++ traning1.py
@@ -1,3 +1,10 @@
 print (8 / 2 + 5 - -3 / 2)
 print ("\"Khal Drogo's favorite word is \"athjahakar\"\"")
 print ("Winter" + " " + "came" + " " + "for" + " " + "the" + " " + "House" + " " + "of" + " " + "Frey"".")
+word = ("First text")
+print (word)
+word = ("Second text")
+print (word)
+
+a = raw_input("What is your name? ")
+print ("Helo," " " + a +"!")
